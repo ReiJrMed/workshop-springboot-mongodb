06fb6f8262bd424bd97b8a877d5af8917ba083e2
ReiJrMed <reijrspy@gmail.com>
2020-07-29 15:11:11 -0300

    Consulta simples com @Query

--- src/main/java/com/empresa/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/empresa/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.empresa.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.empresa.workshopmongo.domain.Post;
@@ -10,6 +11,12 @@ import com.empresa.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
     //MongoRepository<Classe, tipo do Id dela>
+	
+	//o @Query indica que será gerada uma consulta no MongoDB no formato Json
+	//{ <field>: { $regex: /pattern/, $options: '<options>' } }, o '?0' aponta o 1º parâmetro, o '?1' o 2º e assim por diante
+	//o 'i' no options indica ignore case, já o regex indica o parâmetro
+	@Query("{'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	//Query method
 	List<Post> findByTitleContainingIgnoreCase(String text);
